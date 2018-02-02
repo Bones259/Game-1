9137b798a454299760d8120d8fc194331f139ed5
Bones259 <philipj2009@live.com>
2018-02-02 14:49:58 +0000

    Added Functions to Level
    Added moving object script and restart level script to cube.

---
+++ Unity Files/Assets/Scripts/MovingObject.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingObject : MonoBehaviour {
+	//Floats for Variables
+	public float min=2f;
+	public float max=3f;
+	// Use this for initialization
+	void Start () {
+		min=transform.position.x;
+		//Change Number on next line to change distance covered
+		//Each tile in game equals around 2.5
+		max=transform.position.x+10;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//Change Number next to Time.time to increase speed
+		transform.position =new Vector3(Mathf.PingPong(Time.time*2,max-min)+min, transform.position.y, transform.position.z);
+	}
+}
